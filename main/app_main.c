23974901bfb8ac366a7856c3429edae2f0b309de
Vadim Tkachenko <vt@seriousinsights.com>
2020-04-04 15:48:38 -0700

    Now initializing 1-Wire before establishing network connectivity (#4)

--- main/app_main.c
+++ main/app_main.c
@@ -220,13 +220,14 @@ void app_main(void)
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
+    onewire_start();
+
     /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
      * Read "Establishing Wi-Fi or Ethernet Connection" section in
      * examples/protocols/README.md for more information about this function.
      */
     ESP_ERROR_CHECK(example_connect());
 
-    onewire_start();
     mqtt_start();
     onewire_poll();
 }
