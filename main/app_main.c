5c70e4bcee461c6b07a1021abfcb87934120014d
Vadim Tkachenko <vt@seriousinsights.com>
2020-04-03 23:16:00 -0700

    Now searching for 1-Wire devices at startup (#4)

--- main/app_main.c
+++ main/app_main.c
@@ -3,6 +3,8 @@
  *
  * https://www.homeclimatecontrol.com/ - the Home Climate Control project family
  * https://github.com/home-climate-control/hcc-esp32 - this project on GitHub
+ *
+ * 1-Wire code based on https://github.com/DavidAntliff/esp32-ds18b20-example
  */
 
 #include <stdio.h>
@@ -28,8 +30,69 @@
 #include "esp_log.h"
 #include "mqtt_client.h"
 
+#include "owb.h"
+#include "owb_rmt.h"
+#include "ds18b20.h"
+
+#define GPIO_DS18B20_0       (CONFIG_ONE_WIRE_GPIO)
+#define MAX_DEVICES          (8)
+#define DS18B20_RESOLUTION   (DS18B20_RESOLUTION_12_BIT)
+#define SAMPLE_PERIOD        (1000)   // milliseconds
+
 static const char *TAG = "hcc-esp32";
 
+static void onewire_start(void)
+{
+    // To debug OWB, use 'make menuconfig' to set default Log level to DEBUG, then uncomment:
+    //esp_log_level_set("owb", ESP_LOG_DEBUG);
+
+    // Stable readings require a brief period before communication
+    vTaskDelay(2000.0 / portTICK_PERIOD_MS);
+
+    // Create a 1-Wire bus, using the RMT timeslot driver
+    OneWireBus *owb;
+    owb_rmt_driver_info rmt_driver_info;
+    owb = owb_rmt_initialize(&rmt_driver_info, GPIO_DS18B20_0, RMT_CHANNEL_1, RMT_CHANNEL_0);
+
+    // enable CRC check for ROM code
+    owb_use_crc(owb, true);
+
+    ESP_LOGI(TAG, "Looking for connected 1-Wire devices on pin %d...", GPIO_DS18B20_0);
+
+    OneWireBus_ROMCode device_rom_codes[MAX_DEVICES] = {0};
+    int devices_found = 0;
+    OneWireBus_SearchState search_state = {0};
+    bool found = false;
+    owb_search_first(owb, &search_state, &found);
+    while (found) {
+        char rom_code_s[17];
+        owb_string_from_rom_code(search_state.rom_code, rom_code_s, sizeof(rom_code_s));
+        ESP_LOGI(TAG, "  %d: %s", devices_found, rom_code_s);
+        device_rom_codes[devices_found] = search_state.rom_code;
+        ++devices_found;
+        owb_search_next(owb, &search_state, &found);
+    }
+    ESP_LOGI(TAG, "Found %d device%s", devices_found, devices_found == 1 ? "" : "s");
+
+    // Create DS18B20 devices on the 1-Wire bus
+    DS18B20_Info *devices[MAX_DEVICES] = {0};
+    for (int i = 0; i < devices_found; ++i) {
+        DS18B20_Info *ds18b20_info = ds18b20_malloc();
+        devices[i] = ds18b20_info;
+
+        if (devices_found == 1) {
+            ESP_LOGD(TAG, "Single device optimizations enabled");
+            ds18b20_init_solo(ds18b20_info, owb);
+        } else {
+            // associate with bus and device
+            ds18b20_init(ds18b20_info, owb, device_rom_codes[i]);
+        }
+
+        // enable CRC check for temperature readings
+        ds18b20_use_crc(ds18b20_info, true);
+        ds18b20_set_resolution(ds18b20_info, DS18B20_RESOLUTION);
+    }
+}
 
 static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
 {
@@ -86,7 +149,7 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
     mqtt_event_handler_cb(event_data);
 }
 
-static void mqtt_app_start(void)
+static void mqtt_start(void)
 {
     esp_mqtt_client_config_t mqtt_cfg = {
         .uri = CONFIG_BROKER_URL,
@@ -121,5 +184,6 @@ void app_main(void)
      */
     ESP_ERROR_CHECK(example_connect());
 
-    mqtt_app_start();
+    onewire_start();
+    mqtt_start();
 }
