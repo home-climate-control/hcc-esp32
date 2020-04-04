c897b3c3ecbf2932ede5e8fd345703938cd904a4
Vadim Tkachenko <vt@seriousinsights.com>
2020-04-04 16:04:05 -0700

    Improved log formatting

--- main/app_main.c
+++ main/app_main.c
@@ -52,7 +52,7 @@ static void log_configuration(void)
     ESP_LOGI(TAG, "[conf] MQTT pub root: %s", CONFIG_BROKER_PUB_ROOT);
     ESP_LOGI(TAG, "[conf] MQTT sub root: %s", CONFIG_BROKER_SUB_ROOT);
     ESP_LOGI(TAG, "[conf] 1-Wire GPIO pin: %d", CONFIG_ONE_WIRE_GPIO);
-    ESP_LOGI(TAG, "[conf] 1-Wire sampling interval: %d", CONFIG_ONE_WIRE_POLL_SECONDS);
+    ESP_LOGI(TAG, "[conf] 1-Wire sampling interval: %ds", CONFIG_ONE_WIRE_POLL_SECONDS);
 }
 
 static void onewire_start(void)
@@ -69,7 +69,7 @@ static void onewire_start(void)
     // enable CRC check for ROM code
     owb_use_crc(owb, true);
 
-    ESP_LOGI(TAG, "Looking for connected 1-Wire devices on pin %d...", GPIO_DS18B20_0);
+    ESP_LOGI(TAG, "[1-Wire] looking for connected devices on pin %d...", GPIO_DS18B20_0);
 
     OneWireBus_ROMCode device_rom_codes[MAX_DEVICES] = {0};
     OneWireBus_SearchState search_state = {0};
@@ -78,12 +78,12 @@ static void onewire_start(void)
     while (found) {
         char rom_code_s[17];
         owb_string_from_rom_code(search_state.rom_code, rom_code_s, sizeof(rom_code_s));
-        ESP_LOGI(TAG, "  %d: %s", devices_found, rom_code_s);
+        ESP_LOGI(TAG, "[1-Wire] %d: %s", devices_found, rom_code_s);
         device_rom_codes[devices_found] = search_state.rom_code;
         ++devices_found;
         owb_search_next(owb, &search_state, &found);
     }
-    ESP_LOGI(TAG, "Found %d device%s", devices_found, devices_found == 1 ? "" : "s");
+    ESP_LOGI(TAG, "[1-Wire] found %d device%s", devices_found, devices_found == 1 ? "" : "s");
 
     // Create DS18B20 devices on the 1-Wire bus
     for (int i = 0; i < devices_found; ++i) {
@@ -91,7 +91,7 @@ static void onewire_start(void)
         devices[i] = ds18b20_info;
 
         if (devices_found == 1) {
-            ESP_LOGD(TAG, "Single device optimizations enabled");
+            ESP_LOGD(TAG, "[1-Wire] single device optimizations enabled");
             ds18b20_init_solo(ds18b20_info, owb);
         } else {
             // associate with bus and device
@@ -213,9 +213,9 @@ static void mqtt_start(void)
 
 void app_main(void)
 {
-    ESP_LOGI(TAG, "[APP] Oh, hai");
-    ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
-    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+    ESP_LOGI(TAG, "[core] Oh, hai");
+    ESP_LOGI(TAG, "[core] free memory: %d bytes", esp_get_free_heap_size());
+    ESP_LOGI(TAG, "[core] IDF version: %s", esp_get_idf_version());
 
     esp_log_level_set("*", ESP_LOG_INFO);
     esp_log_level_set("MQTT_CLIENT", ESP_LOG_VERBOSE);
